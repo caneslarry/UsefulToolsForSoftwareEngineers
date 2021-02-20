2a580670ce05ab279d2ac3e75f517173781da1d3
larry <caneslarry@gmail.com>
2021-02-20 10:47:43 -0500

    - Move Class to its own file.

--- mvc/index.php
+++ mvc/index.php
@@ -1,22 +1,6 @@
 <?php
-/**
- * Class FooController
- */
-class FooController {
-    /**
-     * FooController constructor.
-     */
-    public function __construct() {}
 
-    /**
-     * barAction - Sample controller action.
-     * Called from browser like this
-     * ~/Foo/bar
-     */
-    public function barAction() {
-        echo 'Foo bard';
-    }
-}
+include "FooController.php";
 
 $defaultController = "home";
 $defaultAction = "index";
